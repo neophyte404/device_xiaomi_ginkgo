1f11805dfcf370007859b1760d689654b09ba073
Tim Zimmermann <tim@linux4.de>
2021-11-06 16:03:29 +0000

    ginkgo: Flash dummy super image if needed
    * Needed because stock does not use dynamic partitions so
      there's no existing metadata that could be used
    
    Change-Id: Icbc828389465f3d4681ca8e4c3adec782540752e

--- device.mk
+++ device.mk
@@ -168,6 +168,9 @@ TARGET_BOARD_FASTBOOT_INFO_FILE := $(DEVICE_PATH)/fastboot-info.txt
 PRODUCT_PACKAGES += \
     fastbootd
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.ginkgo
