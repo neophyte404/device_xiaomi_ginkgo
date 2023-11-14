c778e382807958282b870466c6fd8db643234449
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-11-14 10:28:53 +0530

    ginkgo: Checkout releasetools.py from davinci
    Change-Id: Ib632510139d94101a99523c3194eaab6fb9147cf

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,7 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2009 The Android Open Source Project
+# Copyright (C) 2019 The Mokee Open Source Project
+# Copyright (C) 2020-2021 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,25 +14,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 import common
+import re
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
+  return
 
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
+  return
 
 def AddImage(info, basename, dest):
-  path = "IMAGES/" + basename
+  name = basename
+  path = "IMAGES/" + name
   if path not in info.input_zip.namelist():
     return
 
   data = info.input_zip.read(path)
-  common.ZipWriteStr(info.output_zip, basename, data)
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
-  info.script.Print("Patching device-tree and verity images...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  return
