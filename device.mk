Alpannnium/device_xiaomi_ginkgo-Al

ginkgo: Build MiuiCamera only if it exists

device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, vendor/xiaomi/ginkgo/ginkgo-vendor.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, drvice/xiaomi/miuicamera-ginkgo/device.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \

neophyte404/device_xiaomi_ginkgo

Signed-off-by: neophyte <neophyteplayground@gmail.com>
Signed-off-by: neophyte404 <260553983+neophyte404@users.noreply.github.com>

device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, vendor/xiaomi/ginkgo/ginkgo-vendor.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# MiuiCamera
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-ginkgo/device.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \