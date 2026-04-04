13a8b12025e9dee04b05a091b3900a0a06429a46
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-04-04 16:11:20 +0000

    ginkgo: Adapt to @PixelOS-AOSP

--- custom_ginkgo.mk
+++ custom_ginkgo.mk
@@ -9,12 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-PRODUCT_NAME := lineage_ginkgo
+PRODUCT_NAME := custom_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
