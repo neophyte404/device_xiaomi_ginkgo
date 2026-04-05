348dddd50b9d9d60deccbb74c6a2a27f15efba0a
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-04-06 00:51:24 +0700

    Revert "ginkgo: Update some soong config variables to bool type"
    This reverts commit 7e574e34a204e1809a8f326e50448c66b2e0bff3.

--- device.mk
+++ device.mk
@@ -167,7 +167,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sdm
 
-$(call soong_config_set_bool,livedisplay_sdm,enable_dm,false)
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
 
 # Media configs
 PRODUCT_COPY_FILES += \
