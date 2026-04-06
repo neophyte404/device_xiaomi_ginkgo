d3a0f5118028165cf67f95452b1cd3281bbdf186
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-04-06 09:48:18 +0700

    Revert "sm6125-common: Remove vendor/lineage device framework matrix inclusion"
    This reverts commit a6a3e58eeb75cbe571a00cafcc746066aade6a51.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,7 +49,8 @@ DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_mani
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
