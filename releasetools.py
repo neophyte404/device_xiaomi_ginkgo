68c6a4c94e9655d2f0466b2f3aafe3582d8a05c5
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-04-24 15:53:54 +0000

    ginkgo: Chekout releasetools.py from LineageOS
    Signed-off-by: neophyte <neophyteplayground@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2009 The Android Open Source Project
-# Copyright (c) 2011, The Linux Foundation. All rights reserved.
+#
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,34 +14,23 @@
 # limitations under the License.
 
 import common
-import re
-
-def FullOTA_Assertions(info):
-  input_zip = info.input_zip
-  return
-
-def IncrementalOTA_Assertions(info):
-  input_zip = info.target_zip
-  return
 
 def FullOTA_InstallEnd(info):
-  input_zip = info.input_zip
-  OTA_InstallEnd(info, input_zip)
-  return
+  OTA_InstallEnd(info)
 
 def IncrementalOTA_InstallEnd(info):
-  input_zip = info.target_zip
-  OTA_InstallEnd(info, input_zip)
-  return
+  OTA_InstallEnd(info)
 
-def AddImage(info, input_zip, basename, dest):
-  name = basename
-  data = input_zip.read("IMAGES/" + basename)
-  common.ZipWriteStr(info.output_zip, name, data)
-  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+def AddImage(info, basename, dest):
+  path = "IMAGES/" + basename
+  if path not in info.input_zip.namelist():
+    return
 
-def OTA_InstallEnd(info, input_zip):
-  AddImage(info, input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
-  AddImage(info, input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  return
+  data = info.input_zip.read(path)
+  common.ZipWriteStr(info.output_zip, basename, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+
+def OTA_InstallEnd(info):
+  info.script.Print("Patching device-tree and verity images...")
+  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
