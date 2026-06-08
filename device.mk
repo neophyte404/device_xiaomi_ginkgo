fac64aeb36c4a5c48f4c34e1cc5b0449f0b58311
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-06-08 11:07:40 +0000

    ginkgo: Drop Dolby Support

--- device.mk
+++ device.mk
@@ -247,13 +247,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
-# Dolby
-$(call inherit-product, hardware/dolby/dolby.mk)
-
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    DSPVolumeSynchronizer
-
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
